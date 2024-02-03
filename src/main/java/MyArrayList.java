706201b1ba6dcd0d63f764948c66858a9a033496
ZzSerg <azuraka@mail.ru>
2024-02-04 01:20:15 +0500

    feat: реализовано домашнее задание :)

---
+++ src/main/java/MyArrayList.java
@@ -0,0 +1,250 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MyArrayList<T> implements MyList<T> {
+
+    private enum MoveDirection {FORWARD, BACKWARD}
+
+    private final int INITIAL_CAPACITY = 16;
+    private Object[] elements;
+    private int lastIndex;
+    private Comparator<? super T> comparator;
+    private final Comparator<T> hashComparator = Comparator.comparingInt(Object::hashCode);
+
+    public MyArrayList() {
+        arrayInit(0);
+    }
+
+    public MyArrayList(Comparator<? super T> comparator) {
+        this.comparator = comparator;
+        arrayInit(0);
+    }
+
+    public MyArrayList(int initialCapacity) {
+        arrayInit(initialCapacity);
+    }
+
+    public MyArrayList(int initialCapacity, Comparator<? super T> comparator) {
+        this.comparator = comparator;
+        arrayInit(initialCapacity);
+    }
+
+    private void arrayInit(int capacity) {
+        if (capacity > INITIAL_CAPACITY) {
+            elements = new Object[capacity];
+        } else
+            elements = new Object[INITIAL_CAPACITY];
+
+        lastIndex = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        if (element == null){
+            throw new UnsupportedOperationException();
+        }
+
+        if (checkSize()) {
+            grow();
+        }
+        elements[lastIndex++] = element;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        if (element == null){
+            throw new UnsupportedOperationException();
+        }
+
+        checkIndex(index);
+
+        if (checkSize()) {
+            grow();
+        }
+
+        movePartOfArray(index, MoveDirection.FORWARD);
+        elements[index] = element;
+        lastIndex++;
+    }
+
+    private boolean checkSize() {
+        return lastIndex == elements.length - 1;
+    }
+
+    private void grow() {
+        Object[] objects = new Object[elements.length * 2];
+        System.arraycopy(
+                elements, 0,
+                objects, 0, elements.length);
+
+        elements = objects;
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return getElement(index);
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= lastIndex) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private T getElement(int index) {
+        checkIndex(index);
+        return (T) elements[index];
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+
+        T element = get(index);
+
+        if (element != null) {
+            movePartOfArray(index, MoveDirection.BACKWARD);
+            lastIndex--;
+            return element;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        for (int i = 0; i < lastIndex; i++) {
+            T elem = getElement(i);
+            if (element.equals(elem)) {
+                movePartOfArray(i, MoveDirection.BACKWARD);
+                lastIndex--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void replace(int index, T element) {
+        checkIndex(index);
+        elements[index] = element;
+    }
+
+    private void movePartOfArray(int index, MoveDirection direction) {
+        switch (direction) {
+            case FORWARD -> System.arraycopy(
+                    elements, index,
+                    elements, index + 1,
+                    lastIndex - index + 1);
+
+            case BACKWARD -> System.arraycopy(
+                    elements, index + 1,
+                    elements, index,
+                    lastIndex - index);
+        }
+    }
+
+    @Override
+    public void clear() {
+        lastIndex = 0;
+    }
+
+    @Override
+    public void sort() {
+        if (comparator == null) {
+            comparator = hashComparator;
+        }
+        sortElements(0, lastIndex - 1);
+    }
+
+    private void sortElements(int left, int right) {
+        int length = right - left;
+        if (length > 0) {
+
+            if (length <= 11) {
+                insertionSort(left, right);
+            } else {
+                int pivotPosAfterSort = miniSort(left, right);
+
+                sortElements(left, pivotPosAfterSort - 1);
+                sortElements(pivotPosAfterSort + 1, right);
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void insertionSort(int leftPos, int rightPos) {
+        for (int out = leftPos + 1; out <= rightPos; out++) {
+            T obj = (T) elements[out];
+            int mark = out;
+
+            while (mark > leftPos && comparator.compare((T) elements[mark - 1], obj) > 0) {
+                elements[mark] = elements[mark - 1];
+                mark--;
+            }
+
+            elements[mark] = obj;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void normalize(int leftPos, int rightPos) {
+        T leftElem = (T) elements[leftPos];
+        T rightElem = (T) elements[rightPos];
+
+        if (comparator.compare(leftElem, rightElem) > 0) {
+            swap(leftPos, rightPos);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private int miniSort(int left, int right) {
+        int leftPos = left -1;
+        int rightPos = right;
+        T pivotElement = (T) elements[right];
+
+        while (true) {
+            while (comparator.compare((T) elements[++leftPos], pivotElement) < 0);
+
+            while (rightPos > 0 && comparator.compare((T) elements[--rightPos], pivotElement) > 0);
+
+            if (leftPos >= rightPos) {
+                break;
+            } else {
+                swap(leftPos, rightPos);
+            }
+        }
+        swap(leftPos, right);
+        return leftPos;
+    }
+
+    private void swap(int left, int right) {
+        Object temp = elements[left];
+        elements[left] = elements[right];
+        elements[right] = temp;
+    }
+
+
+    @Override
+    public int size() {
+        return lastIndex;
+    }
+
+    @Override
+    public boolean contains(T element) {
+        for (int i = 0; i < lastIndex; i++) {
+            T elem = getElement(i);
+            if (element.equals(elem)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return
+                Arrays.toString(Arrays.copyOf(elements, lastIndex));
+    }
+}
---
+++ src/test/java/MyArrayListTest.java
@@ -0,0 +1,263 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MyArrayListTest {
+
+    private Random random = new Random();
+    private String testStr = "Безумно, можно быть первым Безумно, можно через стены Я шерстяной волчара, боже, как я хорош, как мощны мои лапищи Редко когда думаю, а еще реже думаю АУФ";
+
+
+    private static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Person person = (Person) o;
+            return age == person.age && Objects.equals(name, person.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+    @Test
+    void add() {
+        MyList<String> myStringList = new MyArrayList<>();
+        assertEquals(0, myStringList.size());
+        for (int i = 0; i < 10; i++) {
+            myStringList.add(getRandomString());
+        }
+        assertEquals(10, myStringList.size());
+    }
+
+    @Test
+    void addByIndex() {
+        MyList<String> myStringList = new MyArrayList<>();
+        assertEquals(0, myStringList.size());
+        for (int i = 0; i < 10; i++) {
+            myStringList.add(getRandomString());
+        }
+        assertEquals(10, myStringList.size());
+
+        for (int i = 0; i < 10; i++) {
+            myStringList.add(5, "index 5");
+        }
+        assertEquals(20, myStringList.size());
+        for (int i = 5; i < 15; i++) {
+            assertEquals(myStringList.get(i), "index 5");
+        }
+    }
+
+    @Test
+    void get() {
+        MyList<String> myStringList = new MyArrayList<>();
+        assertEquals(0, myStringList.size());
+        for (int i = 0; i < 10; i++) {
+            myStringList.add(getRandomString());
+        }
+        assertEquals(10, myStringList.size());
+
+        for (int i = 0; i < myStringList.size(); i++) {
+            assertNotNull(myStringList.get(i));
+            assertTrue(myStringList.get(i).length() > 0);
+        }
+    }
+
+    @Test
+    void removes() {
+        MyList<Person> personList = new MyArrayList<>();
+        Person boy = new Person("Школоло", 12);
+        Person oldMan = new Person("Старец", 100);
+        Person dunkan = new Person("Дункан", 564);
+
+        for (int i = 0; i < 10; i++) {
+            personList.add(new Person(getRandomString(), random.nextInt(100)));
+        }
+        assertEquals(10, personList.size());
+
+        personList.add(boy);
+        personList.add(5, oldMan);
+        personList.add(7, dunkan);
+
+        assertTrue(personList.contains(boy));
+        assertTrue(personList.contains(oldMan));
+        assertTrue(personList.contains(dunkan));
+
+        personList.remove(personList.size()-1);
+        personList.remove(7);
+        personList.remove(5);
+
+        assertFalse(personList.contains(boy));
+        assertFalse(personList.contains(oldMan));
+        assertFalse(personList.contains(dunkan));
+
+
+        personList.add(2, boy);
+        personList.add(6, oldMan);
+        personList.add(8, dunkan);
+
+        assertTrue(personList.contains(boy));
+        assertTrue(personList.contains(oldMan));
+        assertTrue(personList.contains(dunkan));
+
+        personList.remove(oldMan);
+        personList.remove(boy);
+        personList.remove(dunkan);
+
+        assertFalse(personList.contains(boy));
+        assertFalse(personList.contains(oldMan));
+        assertFalse(personList.contains(dunkan));
+    }
+
+    @Test
+    void replace(){
+        MyList<Person> personList = new MyArrayList<>();
+        Person boy = new Person("Школоло", 12);
+        Person oldMan = new Person("Старец", 100);
+        Person dunkan = new Person("Дункан", 564);
+
+        personList.add(boy);
+        personList.add(oldMan);
+        personList.add(dunkan);
+
+        assertTrue(personList.contains(boy));
+        assertTrue(personList.contains(oldMan));
+        assertTrue(personList.contains(dunkan));
+
+        var randomPerson = new Person(getRandomString(), random.nextInt(100));
+        personList.replace(1, randomPerson);
+        assertFalse(personList.contains(oldMan));
+        assertTrue(personList.contains(randomPerson));
+    }
+
+    @Test
+    void clear() {
+        MyList<Integer> integerMyList = new MyArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            integerMyList.add(i);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            integerMyList.remove(i);
+        }
+        assertEquals(50, integerMyList.size());
+
+        integerMyList.clear();
+        assertEquals(0, integerMyList.size());
+    }
+
+    @Test
+    void sort() {
+        //integer sort
+        MyList<Integer> integerMyList = new MyArrayList<>();
+        integerMyList.add(10);
+        integerMyList.add(6);
+        integerMyList.add(1);
+        integerMyList.add(8);
+        integerMyList.add(3);
+        integerMyList.add(2);
+        integerMyList.add(5);
+        integerMyList.add(0);
+        integerMyList.add(7);
+        integerMyList.add(9);
+        integerMyList.add(4);
+
+        integerMyList.sort();
+
+        for (int i = 0; i < integerMyList.size(); i++) {
+            assertEquals(i, integerMyList.get(i));
+        }
+
+
+        //classes comparator sort
+        MyList<Person> persons = new MyArrayList<>(Comparator.comparing(Person::getName));
+        persons.add(new Person("r", 1));
+        persons.add(new Person("f", 1));
+        persons.add(new Person("b", 1));
+        persons.add(new Person("c", 1));
+        persons.add(new Person("a", 1));
+        persons.add(new Person("e", 1));
+        persons.add(new Person("g", 1));
+        persons.add(new Person("d", 1));
+        persons.sort();
+
+        assertEquals("a", persons.get(0).name);
+        assertEquals("b", persons.get(1).name);
+        assertEquals("c", persons.get(2).name);
+        assertEquals("d", persons.get(3).name);
+        assertEquals("e", persons.get(4).name);
+        assertEquals("f", persons.get(5).name);
+        assertEquals("g", persons.get(6).name);
+        assertEquals("r", persons.get(7).name);
+
+
+
+
+
+        //classes hash sort
+        MyList<Person> personsHash = new MyArrayList<>();
+        personsHash.add(new Person("r", 1));
+        personsHash.add(new Person("f", 1));
+        personsHash.add(new Person("b", 1));
+        personsHash.add(new Person("c", 1));
+        personsHash.add(new Person("a", 1));
+        personsHash.add(new Person("e", 1));
+        personsHash.add(new Person("g", 1));
+        personsHash.add(new Person("d", 1));
+
+        persons.sort();
+
+        assertEquals("a", persons.get(0).name);
+        assertEquals("b", persons.get(1).name);
+        assertEquals("c", persons.get(2).name);
+        assertEquals("d", persons.get(3).name);
+        assertEquals("e", persons.get(4).name);
+        assertEquals("f", persons.get(5).name);
+        assertEquals("g", persons.get(6).name);
+        assertEquals("r", persons.get(7).name);
+    }
+
+    private String getRandomString(){
+        int start = random.nextInt(testStr.length());
+        int end = random.nextInt(testStr.length());
+
+        if (start >= end){
+            return getRandomString();
+        }
+
+        return testStr.substring(start, end);
+    }
+}
